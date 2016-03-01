7b4701b6c1b3fec4eeee581af34da0fff24f79dd
hwiceberg <weihong@Weis-MacBook-Pro.local>
2016-02-29 20:29:05 -0500

    add globals

---
+++ memcache/Globals.h
@@ -0,0 +1,18 @@
+//
+//  Globals.h
+//  MIPS_Simulator2
+//
+//  Created by Wei Hong on 2/29/16.
+//  Copyright (c) 2016 Wei Hong. All rights reserved.
+//
+
+#ifndef __MIPS_Simulator2__Globals__
+#define __MIPS_Simulator2__Globals__
+
+#include <stdio.h>
+#include <random>
+
+int genRandomNumber(int ways);
+extern long global_clock;
+
+#endif /* defined(__MIPS_Simulator2__Globals__) */
