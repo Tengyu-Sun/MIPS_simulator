e3e1dfd0eccd779a71955f20fac388249d96e163
arbiculator <arbiculator@gmail.com>
2016-03-23 16:33:43 -0400

    load opcode map

--- asm/asm.cpp
+++ asm/asm.cpp
@@ -6,37 +6,73 @@
 using namespace std;
 
 map<String, uint32_t> opcode_map = {
-    {"lb",   0b0010000},
-    {"add",  0b1000000},
-    {"sub",  0b1000001},
-    {"bgez", 0b0000101},
-    {"sb",   0b0011000},
-    {"break",0b0000000},
-    {"blez", 0b0000111},
-    {"bltz", 0b0001000},
-    {"addsp",0b0100000},
-    {"subsp",0b0100001},
-    {"mulsp",0b0100010},
-    {"divsp",0b0100011},
-    {"sltsp",0b0100100},
-    {"witf", 0b0100101},
-    {"wfti", 0b0100110},
-    {"pref", 0b0110000},
-    {"move", 0b1101010},
-    {"copys",0b1101011},
-    {"copyu",0b1101100},
-    {"insertb",0b1101101},
-    {"fillb", 0b1101110},
-    {"vaddb", 0b1100000},
-    {"vsubb", 0b1100001},
-    {"vmulb", 0b1100010},
-    {"vdivv", 0b1100011},
-    {"vmodb", 0b1100100},
-    {"ceqb", 0b1100101},
-    {"cleb", 0b1100110},
-    {"cleub", 0b1100111},
-    {"cltb", 0b1101000},
-    {"cltub", 0b1101001},
+    //data transfer
+    {"lb",      0b0010000},
+    {"lbu",     0b0010001},
+    {"sb",      0b0011000},
+    {"lw",      0b0010010},
+    {"sw",      0b0011001},
+    {"lsp",     0b0010100},
+    //arithmetic
+    {"add",     0b1000000},
+    {"sub",     0b1000001},
+    {"addi",    0b1010011},
+    {"subi",    0b1010100},
+    {"mul",     0b1000010},
+    {"muh",     0b1000011},
+    {"mulu",    0b1000100},
+    {"muhu",    0b1000101},
+    {"div",     0b1000110},
+    {"divu",    0b1000111},
+    {"modu",    0b1001000},
+    {"add",     0b1001001},
+    {"or",      0b1001010},
+    {"not",     0b1001011},
+    {"xor",     0b1001100},
+    {"rr",      0b1001101},
+    {"srl",     0b1001110},
+    {"sra",     0b1001111},
+    {"sl",      0b1010000},
+    {"slt",     0b1010001},
+    {"sltu",    0b1010010},
+    {"slti",    0b1010101},
+    {"sltiu",   0b1010110},
+    //control
+    {"j",       0b0000001},
+    {"jal",     0b0000010},
+    {"beq",     0b0000011},
+    {"bneq",    0b0000100},
+    {"bgez",    0b0000101},
+    {"bgtz",    0b0000110},
+    {"blez",    0b0000111},
+    {"bltz",    0b0001000},
+    {"break",   0b0000000},
+    //floating point
+    {"addsp",   0b0100000},
+    {"subsp",   0b0100001},
+    {"mulsp",   0b0100010},
+    {"divsp",   0b0100011},
+    {"sltsp",   0b0100100},
+    {"witf",    0b0100101},
+    {"wfti",    0b0100110},
+    //cache
+    {"pref",    0b0110000},
+    //simd
+    {"move",    0b1101010},
+    {"copys",   0b1101011},
+    {"copyu",   0b1101100},
+    {"insertb", 0b1101101},
+    {"fillb",   0b1101110},
+    {"vaddb",   0b1100000},
+    {"vsubb",   0b1100001},
+    {"vmulb",   0b1100010},
+    {"vdivv",   0b1100011},
+    {"vmodb",   0b1100100},
+    {"ceqb",    0b1100101},
+    {"cleb",    0b1100110},
+    {"cleub",   0b1100111},
+    {"cltb",    0b1101000},
+    {"cltub",   0b1101001},
 };
 
 
@@ -44,51 +80,13 @@ map<String, uint32_t> opcode_map = {
 
 uint32_t encoding(string opcode, vector<string> para) {
   uint32_t code = 0;
-  if (opcode == "lb") {
-    code = 0b0010000;
-  } else if (opcode == "add") {
-    code = 0b1000000;
-  } else if (opcode == "sub") {
-    code = 0b1000001;
-  } else if (opcode == "bgez") {
-    code = 0b0000101;
-  } else if (opcode == "sb") {
-    code = 0b0011000;
-  } else if (opcode == "break") {
-    return 0;
-  } else if (opcode == " ") {
-
-  } else if (opcode == " ") {
-
-  } else if (opcode == " ") {
-
-  } else if (opcode == " ") {
-
-  } else if (opcode == " ") {
-
-  } else if (opcode == " ") {
-
-  } else if (opcode == " ") {
-
-  } else if (opcode == " ") {
-
-
-  } else if (opcode == " ") {
-
-
-  } else if (opcode == " ") {
-
-
-  } else if (opcode == " ") {
-
-  } else if (opcode == " ") {
-
-  } else if (opcode == " ") {
-
-
-  } else {
+  auto entry = opcode_map.find(opcode);
+  if(entry != opcode_map.end()) {
     cout<<"error opcode: "<<opcode<<endl;
     return 0;
+  } else {
+    code = entry->second;
+    if(code == 0) return 0;
   }
 
   if (para.size() == 1) {
