0ee268b68d7542fb3b7e84561992e41c6eb2bfbf
Tengyu-Sun <tsun@cs.umass.edu>
2016-03-23 15:01:09 -0400

    vu

--- simulator/vu.cpp
+++ simulator/vu.cpp
@@ -5,7 +5,7 @@ VU::VU(int cycle_): cycle(cycle_), countdown(cycle_) {
 }
 
 int VU::vuCal(uint64_t A, uint64_t B, uint64_t *C, int opcode) {
-  if(opcode < 0 || opcode > 4) {
+  if(opcode < 0 || opcode > 9) {
     std::cout<<"vuCal: error opcode"<<std::endl;
     return -1;
   }
@@ -13,8 +13,56 @@ int VU::vuCal(uint64_t A, uint64_t B, uint64_t *C, int opcode) {
     --countdown;
   }
   if (countdown == 0 ) {
-    switch(opcode) {
-      case 0:
+    uint64_t res = 0;
+    uint8_t tmpA[8];
+    uint8_t tmpB[8];
+    uint8_t tmpC[8];
+    for (int i=7; i>=0; --i) {
+      tmpA[i] = A & 0xff;
+      tmpB[i] = B & 0xff;
+      A >>=8;
+      B >>=8;
+    }
+    if (opcode == 0) {
+      for(int i=0; i<8; ++i) {
+        tmpC[i] = tmpA[i] + tmpB[i];
+      }
+    } else if (opcode == 1) {
+      for(int i=0; i<8; ++i) {
+        tmpC[i] = tmpA[i] + tmpB[i];
+      }
+    } else if (opcode == 2) {
+      for(int i=0; i<8; ++i) {
+        tmpC[i] = tmpA[i] + tmpB[i];
+      }
+    } else if (opcode == 3) {
+      for(int i=0; i<8; ++i) {
+        tmpC[i] = tmpA[i] + tmpB[i];
+      }
+    } else if (opcode == 4) {
+      for(int i=0; i<8; ++i) {
+        tmpC[i] = tmpA[i] + tmpB[i];
+      }
+    } else if (opcode == 5) {
+      for(int i=0; i<8; ++i) {
+        tmpC[i] = tmpA[i] + tmpB[i];
+      }
+    } else if (opcode == 6) {
+      for(int i=0; i<8; ++i) {
+        tmpC[i] = tmpA[i] + tmpB[i];
+      }
+    } else if (opcode == 7) {
+      for(int i=0; i<8; ++i) {
+        tmpC[i] = tmpA[i] + tmpB[i];
+      }
+    } else if (opcode == 8) {
+      for(int i=0; i<8; ++i) {
+        tmpC[i] = tmpA[i] + tmpB[i];
+      }
+    } else if (opcode == 9) {
+      for(int i=0; i<8; ++i) {
+        tmpC[i] = tmpA[i] + tmpB[i];
+      }
     }
   } else {
     return 0;
