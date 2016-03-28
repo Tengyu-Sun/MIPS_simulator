23e2761c8066f24f5f3c25503067a19e280cecf4
Tengyu-Sun <tsun@cs.umass.edu>
2016-03-28 11:14:17 -0400

    matrix mulplication

--- asm/matrix_mulplication.asm
+++ asm/matrix_mulplication.asm
@@ -1,31 +1,31 @@
 #allocate memory for matrices
 #left matrix
   addi $0,$1,1
-  sw $0,$1,100
+  sb $0,$1,500
   addi $0,$1,2
-  sw $0,$1,101
+  sb $0,$1,501
   addi $0,$1,3
-  sw $0,$1,102
+  sb $0,$1,502
   addi $0,$1,4
-  sw $0,$1,103
+  sb $0,$1,503
 #right matrix
   addi $0,$1,5
-  sw $0,$1,104
+  sb $0,$1,504
   addi $0,$1,6
-  sw $0,$1,105
+  sb $0,$1,505
   addi $0,$1,7
-  sw $0,$1,106
+  sb $0,$1,506
   addi $0,$1,8
-  sw $0,$1,107
+  sb $0,$1,507
 #result matrix
   addi $0,$1,0
-  sw $0,$1,108
+  sb $0,$1,508
   addi $0,$1,0
-  sw $0,$1,109
+  sb $0,$1,509
   addi $0,$1,0
-  sw $0,$1,110
+  sb $0,$1,510
   addi $0,$1,0
-  sw $0,$1,111
+  sb $0,$1,511
 # lM[a][b]*rM[b][c]=res[a][c]
 # $1 = a
 # $2 = b
@@ -50,41 +50,41 @@ BEGINFOR2:
   bgez $10,ENDFOR2
   addi $0,$6,0
 # start loop 3
-# $16 = res[i][j] = 0
-  addi $0,$16,0
+# $9 = res[i][j] = 0
+  addi $0,$9,0
 BEGINFOR3:
   sub $6,$3,$10
   bgez $10,ENDFOR3
 # res[i][j] += lM[i][k]*rM[k][j]
 # address of matrix[i][j] = i*row_length + j + start_address
-# $11 = i*b + k 
-  mul $4,$2,$11
-  add $6,$11,$11
-# $12 = lM[i][k]  
-  lw $11,$12,100
-# $13 = k*c + j
-  mul $6,$3,$13
-  add $5,$13,$13
-# $14 = rM[k][j]
-  lw $13,$14,104
-# $15 = lM[i][k]*rM[k][j]
-  mul $12,$14,$15
-# $16 += lM[i][k]*rM[k][j]
-  add $15,$16,$16
+# $10 = i*b + k
+  mul $4,$2,$10
+  add $6,$10,$10
+# $7 = lM[i][k]
+  lb $10,$7,500
+# $10 = k*c + j
+  mul $6,$3,$10
+  add $5,$10,$10
+# $8 = rM[k][j]
+  lb $10,$8,504
+# $10 = lM[i][k]*rM[k][j]
+  mul $7,$8,$10
+# $9 += lM[i][k]*rM[k][j]
+  add $10,$9,$9
 # k = k + 1
   addi $6,$6,1
-  j BEGINFOR3
+  beq $0,$0,BEGINFOR3
 ENDFOR3:
-# write $16 to res[i][j]
+# write $9 to res[i][j]
   mul $4,$2,$10
   add $5,$10,$10
-  lw $10,$16,108
+  sb $10,$9,508
 # j = j + 1
   addi $5,$5,1
-  j BEGINFOR2
+  beq $0,$0,BEGINFOR2
 ENDFOR2:
 # i = i + 1
   addi $4,$4,1
-  j BEGINFOR1
+  beq $0,$0,BEGINFOR1
 ENDFOR1:
   break
